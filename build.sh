b1cfdb55fec6db67f33f1dc1321932a427f77e05
Faizulafnaz <faizulafnaz22@gmail.com>
2025-08-20 12:55:44 +0530

    build command updated

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@gamil.com').exists() or User.objects.create_superuser( 'admin@gmail.com', 'admin@123')"
 python manage.py populate_sample_data
