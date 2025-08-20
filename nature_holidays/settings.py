6d38a7447728820536ba695b685d6a975635efaf
Faizulafnaz <faizulafnaz22@gmail.com>
2025-08-20 14:34:14 +0530

    added imaged in about and added the unfold

--- nature_holidays/settings.py
+++ nature_holidays/settings.py
@@ -34,6 +34,7 @@ else:
     
     # Application definition
     INSTALLED_APPS = [
+        'unfold',
         'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
